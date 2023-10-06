ce2d4f6dedadf033513373f1a6f0eeee58aa873e
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-10-06 06:14:12 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Header from "./layout/Navbar";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
